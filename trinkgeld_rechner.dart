129521472dd76344c60b07cf48d1263a685d9073
Shokri Francis Raoof <raoofs.de@gmail.com>
2025-03-07 22:05:38 +0100

    tip calculation and result

--- trinkgeld_rechner.dart
+++ trinkgeld_rechner.dart
@@ -1,21 +1,38 @@
 import 'dart:io';
 
 void main() {
-
-  print("Enter the bill amount: ");
-  String? billAmount = stdin.readLineSync()!;
-
-  print("Choose a tip %: ");
-  String? tipPercentage = stdin.readLineSync()!;
+  double? billAmount;
+  double? tipPercentage;
+  double? tip;
 
   while (true) {
       print("Enter the bill amount: ");
       String? input = stdin.readLineSync();
-      double? billAmount = double.tryParse(input ?? '');
+      billAmount = double.tryParse(input ?? '');
       
       if (billAmount != null) {
+        print("Your bill total is: $billAmount€");
         break; // Exit loop if valid number
       }
       print("Invalid input! Please enter a valid number.");
     }
+
+  while (true) {
+      print("Enter the tip % you would like to leave: ");
+      String? input = stdin.readLineSync();
+      tipPercentage = double.tryParse(input ?? '');
+      
+      if (tipPercentage != null) {
+        print("You want to leave a $tipPercentage% tip");
+        break; // Exit loop if valid number
+      }
+      print("Invalid input! Please enter a valid number.");
+  }
+
+  tip = billAmount * (tipPercentage / 100);
+  print("Your tip is: ${tip.toStringAsFixed(2)}€");
+
+
+
+
 }
